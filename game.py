27d507857e03340cd3493b666833cd3296eef609
Zoey Lee <zoeylee2626@gmail.com>
2024-11-18 19:13:16 -0800

    Refactor a typo and a detail

--- game.py
+++ game.py
@@ -6,12 +6,12 @@ def make_character():
     starting_pokemon = {'Squirtle': {'type': 'water', 'currentHP': 20},
                         'Charmander': {'type': 'fire', 'currentHP': 20},
                         'Bulbasaur': {'type': 'grass', 'currentHP': 20}}
-    print("which poketmon would you like to go together?")
+    print("which pokemon would you like to go together?")
     print("Squirtle(Water) | Charmander(Fire) | Bulbasaur(Grass)")
     user_choice = input("Please type pokemon name: ").capitalize()
     while user_choice not in starting_pokemon:
         print("{} is not included in starting pokemon".format(user_choice))
-        user_choice = input("what pokemon would you like? ")
+        user_choice = input("what pokemon would you like? ").capitalize()
     user_status['poke ball'] = {user_choice: starting_pokemon[user_choice]}
     return user_status
 
