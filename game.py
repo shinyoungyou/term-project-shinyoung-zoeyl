9eb6564f4dd146a528f1cc3d3f9adf75095878e8
Zoey Lee <zoeylee2626@gmail.com>
2024-11-27 10:55:24 -0800

    Refactor fight

--- game.py
+++ game.py
@@ -159,12 +159,12 @@ def skill_result(user_pokemon_skill, skill_collection, event_pokemon_info, chara
         return True
 
 
-def fight(user_pokemon, event_pokemon_info, character):
-    skill_collection = get_skill_of(character['Poke Ball'][user_pokemon]['type'])
+def fight(character_pokemon, event_pokemon_info):
+    skill_collection = get_skill_of(character_pokemon[1]['type'])
     user_pokemon_skill = choose_skill_to_challenge(skill_collection)
     skill_accuracy = get_probability()
     if skill_accuracy:
-        return skill_result(user_pokemon_skill, skill_collection, event_pokemon_info, character)
+        return skill_result(user_pokemon_skill, skill_collection, event_pokemon_info)
     else:
         print(f"\n{user_pokemon_skill} missed!")
         return True
@@ -341,7 +341,7 @@ def select_event_option(event_type):
 
 def proceed_event_option(user_choice, character_pokemon, character, event_pokemon_info):
     if user_choice == fight:
-        process_result = fight(character_pokemon, event_pokemon_info, character)
+        process_result = fight(character_pokemon, event_pokemon_info)
     elif user_choice == throw_poke_ball:
         process_result = throw_poke_ball(event_pokemon_info, character)
     else:
