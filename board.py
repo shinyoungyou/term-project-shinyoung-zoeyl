bbf50b00d7535d949a732aa450856784fe0af2c5
shinyoungyou <austyn.you@gmail.com>
2024-12-03 14:50:38 -0800

    Remove doctests form display_current_location

--- board.py
+++ board.py
@@ -61,29 +61,6 @@ def display_current_location(board: dict[(int, int), bool | str], character: dic
     :precondition: rows is a positive integers
     :precondition: columns is a positive integers
     :postcondition: prints the current location of character, store, and gym
-
-    >>> test_board = {(0, 0): True, (0, 1): True, (0, 2): True, (0, 3): True, (0, 4): True, (0, 5): False,
-    ...              (1, 0): False, (1, 1): True, (1, 2): True, (1, 3): True, (1, 4): True, (1, 5): False,
-    ...              (2, 0): False, (2, 1): True, (2, 2): 'Store', (2, 3): True, (2, 4): True, (2, 5): False,
-    ...              (3, 0): False, (3, 1): True, (3, 2): True, (3, 3): True, (3, 4): True, (3, 5): False,
-    ...              (4, 0): False, (4, 1): True, (4, 2): True, (4, 3): True, (4, 4): True, (4, 5): False,
-    ...              (5, 0): False, (5, 1): True, (5, 2): True, (5, 3): True, (5, 4): True, (5, 5): 'Gym'}
-    >>> test_character = {"Current Location": (0, 0)}
-    >>> display_current_location(test_board, test_character, 6, 6)
-    [🤠][  ][  ][  ][  ]
-        [  ][  ][  ][  ]
-        [  ][💊][  ][  ]
-        [  ][  ][  ][  ]
-        [  ][  ][  ][  ]
-        [  ][  ][  ][  ][🥊]
-    >>> test_character = {"Current Location": (2, 2)}
-    >>> display_current_location(test_board, test_character, 6, 6)
-    [  ][  ][  ][  ][  ]
-        [  ][  ][  ][  ]
-        [  ][🤠][  ][  ]
-        [  ][  ][  ][  ]
-        [  ][  ][  ][  ]
-        [  ][  ][  ][  ][🥊]
     """
     if not board:
         return
