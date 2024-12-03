b295356d2f3699ed20f502028dc2bac8fdb484af
Zoey Lee <zoeylee2626@gmail.com>
2024-12-02 17:19:12 -0800

    Add docstrings to get_attacked

--- game.py
+++ game.py
@@ -662,7 +662,7 @@ def check_total_of_user_pokemons(character: dict, event_pokemon_info: tuple) -> 
     Check if the user has Pokémon fewer than six.
 
     :param character: a dictionary containing information about the character's status
-    :param event_pokemon_info: a tuple containing the event Pokémon's name and a dictionary with its type and current hp
+    :param event_pokemon_info: a tuple containing the event Pokémon's name and a dictionary with its type and current HP
     :precondition: character has a value about 'Poke Ball' key and 'Starting Pokemon' key
     :precondition: the event Pokémon must have an HP 10 or less in the event_pokemon_info
     :precondition: the event Pokémon must have an HP greater than 0 in the event_pokemon_info
@@ -688,7 +688,7 @@ def throw_poke_ball(event_pokemon_info: tuple, character: dict) -> bool:
     """
     Check if the event is completed based on the user's selected 'Throw Poke Ball' option.
 
-    :param event_pokemon_info: a tuple containing the event Pokémon's name and a dictionary with its type and current hp
+    :param event_pokemon_info: a tuple containing the event Pokémon's name and a dictionary with its type and current HP
     :param character: a dictionary containing information about the character's status
     :precondition: the event Pokémon must have an HP greater than 0 in the event_pokemon_info
     :precondition: character has a value about 'Current Level' key, 'Starting Pokemon' key and 'Poke Ball' key
@@ -865,9 +865,9 @@ def proceed_event_option(user_choice: Union[Callable[[], None], str], character_
 
     :param user_choice: a function that represents the option the user wants to do
     :param user_choice: a string representing the user's intention to escape the event
-    :param character_pokemon: a tuple containing the user Pokémon's name and a dictionary with its type and current hp
+    :param character_pokemon: a tuple containing the user Pokémon's name and a dictionary with its type and current HP
     :param character: a dictionary containing information about the character's status
-    :param event_pokemon_info: a tuple containing the event Pokémon's name and a dictionary with its type and current hp
+    :param event_pokemon_info: a tuple containing the event Pokémon's name and a dictionary with its type and current HP
     :param event_type: a string that represents what kind of event occurs
     :precondition: the user Pokémon must have an HP greater than 0 in the character_pokemon
     :precondition: the event Pokémon must have an HP greater than 0 in the event_pokemon_info
@@ -936,7 +936,7 @@ def check_status(character_pokemon: tuple) -> bool:
     """
     Check if the user's Pokémon has fainted.
 
-    :param character_pokemon: a tuple containing the user Pokémon's name and a dictionary with its type and current hp
+    :param character_pokemon: a tuple containing the user Pokémon's name and a dictionary with its type and current HP
     :precondition: character_pokemon represents the status of one of the user's Pokémon in battle
     :postcondition: check the status of user's Pokémon
     :return: a boolean value, false if the user's Pokémon has fainted, true otherwise
@@ -950,7 +950,24 @@ def check_status(character_pokemon: tuple) -> bool:
     return status
 
 
-def get_attacked(event_pokemon_info, event_type, character, character_pokemon):
+def get_attacked(event_pokemon_info: tuple, event_type: str, character: dict, character_pokemon: tuple) -> bool:
+    """
+    Check if the user's Pokémon has fainted and the event is over.
+
+    :param event_pokemon_info: a tuple containing the event Pokémon's name and a dictionary with its type and current HP
+    :param event_type: a string that represents what kind of event occurs
+    :param character: a dictionary containing information about the character's status
+    :param character_pokemon: a tuple containing the user Pokémon's name and a dictionary with its type and current HP
+    :precondition: the user Pokémon must have an HP greater than 0 in the character_pokemon
+    :precondition: character_pokemon represents the status of one of the user's Pokémon in battle
+    :precondition: the event Pokémon must have an HP greater than 0 in the event_pokemon_info
+    :precondition: the user Pokémon must have an HP greater than 0 in the character_pokemon
+    :precondition: character_pokemon represents the status of one of the user's Pokémon in battle
+    :postcondition: set up the skill the event Pokémon will use to attack
+    :postcondition: check if the event Pokémon's skill hit
+    :postcondition: check the status of user's Pokémon
+    :return: a boolean value, false if the event is over due to the user's Pokémon fainting, true otherwise
+    """
     skill_collection = get_skill_of(event_pokemon_info[1]['type'])
     event_pokemon_skill = random.choices(list(skill_collection), k=1)[0]
     damage = int(make_damage_stronger(event_type, character['Current Level']) *
