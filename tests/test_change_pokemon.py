ea472cadbf41498221356e5424caa3e693177dd8
Zoey Lee <zoeylee2626@gmail.com>
2024-12-03 15:30:25 -0800

    Add unit tests for change_pokemon

---
+++ tests/test_change_pokemon.py
@@ -0,0 +1,42 @@
+from unittest import TestCase
+from unittest.mock import patch
+from event_option import change_pokemon
+import io
+
+
+class Test(TestCase):
+
+    @patch('builtins.input', return_value='Luxio')
+    def test_change_pokemon_user_has_pokemon_more_than_1(self, _):
+        pokeball = {'Metapod': {'type': 'grass', 'currentHP': 50}, 'Luxio': {'type': 'electric', 'currentHP': 50}}
+        character_pokemon = ('Metapod', {'type': 'grass', 'currentHP': 50})
+
+        result = change_pokemon(pokeball, character_pokemon)
+        expected = ('Luxio', {'type': 'electric', 'currentHP': 50})
+
+        self.assertEqual(result, expected)
+
+    @patch('builtins.input', side_effect=['Seadra', 'Luxio'])
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_change_pokemon_usr_input_is_wrong(self, mock_output, _):
+        pokeball = {'Metapod': {'type': 'grass', 'currentHP': 50}, 'Luxio': {'type': 'electric', 'currentHP': 50}}
+        character_pokemon = ('Metapod', {'type': 'grass', 'currentHP': 50})
+
+        change_pokemon(pokeball, character_pokemon)
+
+        the_game_printed_this = mock_output.getvalue()
+        expected = '\nSeadra is not included in your Poke Balls or has 0HP\n'
+
+        self.assertIn(expected, the_game_printed_this)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_change_pokemon_user_has_one_pokemon(self, mock_output):
+        pokeball = {'Metapod': {'type': 'grass', 'currentHP': 50}}
+        character_pokemon = ('Metapod', {'type': 'grass', 'currentHP': 50})
+
+        change_pokemon(pokeball, character_pokemon)
+
+        the_game_printed_this = mock_output.getvalue()
+        expected = '\nYou has no pokemon to switch to\n'
+
+        self.assertIn(expected, the_game_printed_this)
