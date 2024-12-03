5387873fa265c4087d727bbe296fca717b0ed4bb
Zoey Lee <zoeylee2626@gmail.com>
2024-12-03 10:12:58 -0800

    Add unit tests for make_stronger

---
+++ tests/test_make_stronger.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from event_option import get_probability, make_stronger
+
+
+class Test(TestCase):
+
+    def test_make_stronger_user_level_is_1(self):
+        actual = make_stronger(1)
+        self.assertEqual(actual, 1)
+
+    def test_make_stronger_user_level_is_2(self):
+        actual = make_stronger(2)
+        self.assertEqual(actual, 1.3)
+
+    def test_make_stronger_user_level_is_3(self):
+        actual = make_stronger(3)
+        self.assertEqual(actual, 1.8)
