57e4c66bb82dc902b5fd8f3f24d8e0a0a5862e89
shinyoungyou <austyn.you@gmail.com>
2024-12-03 17:45:56 -0800

    Create doctests and unit tests for validate_move

---
+++ tests/test_validate_move.py
@@ -0,0 +1,99 @@
+from unittest import TestCase
+
+
+from game import validate_move
+
+
+class TestValidateMove(TestCase):
+
+    def test_validate_move_direction_is_up_at_start(self):
+        board = {(0, 0): True, (0, 1): True, (0, 2): True, (0, 3): True, (0, 4): True, (0, 5): False,
+                 (1, 0): False, (1, 1): True, (1, 2): True, (1, 3): True, (1, 4): True, (1, 5): False,
+                 (2, 0): False, (2, 1): True, (2, 2): 'Store', (2, 3): True, (2, 4): True, (2, 5): False,
+                 (3, 0): False, (3, 1): True, (3, 2): True, (3, 3): True, (3, 4): True, (3, 5): False,
+                 (4, 0): False, (4, 1): True, (4, 2): True, (4, 3): True, (4, 4): True, (4, 5): False,
+                 (5, 0): False, (5, 1): True, (5, 2): True, (5, 3): True, (5, 4): True, (5, 5): 'Gym'}
+        character = {'Character Name': 'user1', 'Money': 30, 'Current Level': 1, 'Potion': 0,
+                     'Current Location': (0, 0),
+                     'Starting Pokemon': 'Squirtle',
+                     'Poke Ball': {
+                         'Squirtle': {'type': 'water', 'currentHP': 40},
+                         'Pichu': {'type': 'electric', 'currentHP': 30},
+                         'Shinx': {'type': 'electric', 'currentHP': 30},
+                         'Mareep': {'type': 'electric', 'currentHP': 30},
+                         'Caterpie': {'type': 'grass', 'currentHP': 30},
+                         'Weedle': {'type': 'grass', 'currentHP': 30},
+                     }}
+        direction = 1
+        expected = (False, None)
+        actual = validate_move(board, character, direction)
+        self.assertEqual(expected, actual)
+
+    def test_validate_move_direction_is_down_at_start(self):
+        board = {(0, 0): True, (0, 1): True, (0, 2): True, (0, 3): True, (0, 4): True, (0, 5): False,
+                 (1, 0): False, (1, 1): True, (1, 2): True, (1, 3): True, (1, 4): True, (1, 5): False,
+                 (2, 0): False, (2, 1): True, (2, 2): 'Store', (2, 3): True, (2, 4): True, (2, 5): False,
+                 (3, 0): False, (3, 1): True, (3, 2): True, (3, 3): True, (3, 4): True, (3, 5): False,
+                 (4, 0): False, (4, 1): True, (4, 2): True, (4, 3): True, (4, 4): True, (4, 5): False,
+                 (5, 0): False, (5, 1): True, (5, 2): True, (5, 3): True, (5, 4): True, (5, 5): 'Gym'}
+        character = {'Character Name': 'user1', 'Money': 30, 'Current Level': 1, 'Potion': 0,
+                     'Current Location': (0, 0),
+                     'Starting Pokemon': 'Squirtle',
+                     'Poke Ball': {
+                         'Squirtle': {'type': 'water', 'currentHP': 40},
+                         'Pichu': {'type': 'electric', 'currentHP': 30},
+                         'Shinx': {'type': 'electric', 'currentHP': 30},
+                         'Mareep': {'type': 'electric', 'currentHP': 30},
+                         'Caterpie': {'type': 'grass', 'currentHP': 30},
+                         'Weedle': {'type': 'grass', 'currentHP': 30},
+                     }}
+        direction = 2
+        expected = (False, None)
+        actual = validate_move(board, character, direction)
+        self.assertEqual(expected, actual)
+
+    def test_validate_move_direction_is_left_at_start(self):
+        board = {(0, 0): True, (0, 1): True, (0, 2): True, (0, 3): True, (0, 4): True, (0, 5): False,
+                 (1, 0): False, (1, 1): True, (1, 2): True, (1, 3): True, (1, 4): True, (1, 5): False,
+                 (2, 0): False, (2, 1): True, (2, 2): 'Store', (2, 3): True, (2, 4): True, (2, 5): False,
+                 (3, 0): False, (3, 1): True, (3, 2): True, (3, 3): True, (3, 4): True, (3, 5): False,
+                 (4, 0): False, (4, 1): True, (4, 2): True, (4, 3): True, (4, 4): True, (4, 5): False,
+                 (5, 0): False, (5, 1): True, (5, 2): True, (5, 3): True, (5, 4): True, (5, 5): 'Gym'}
+        character = {'Character Name': 'user1', 'Money': 30, 'Current Level': 1, 'Potion': 0,
+                     'Current Location': (0, 0),
+                     'Starting Pokemon': 'Squirtle',
+                     'Poke Ball': {
+                         'Squirtle': {'type': 'water', 'currentHP': 40},
+                         'Pichu': {'type': 'electric', 'currentHP': 30},
+                         'Shinx': {'type': 'electric', 'currentHP': 30},
+                         'Mareep': {'type': 'electric', 'currentHP': 30},
+                         'Caterpie': {'type': 'grass', 'currentHP': 30},
+                         'Weedle': {'type': 'grass', 'currentHP': 30},
+                     }}
+        direction = 3
+        expected = (False, None)
+        actual = validate_move(board, character, direction)
+        self.assertEqual(expected, actual)
+
+    def test_validate_move_direction_is_right_at_start(self):
+        board = {(0, 0): True, (0, 1): True, (0, 2): True, (0, 3): True, (0, 4): True, (0, 5): False,
+                 (1, 0): False, (1, 1): True, (1, 2): True, (1, 3): True, (1, 4): True, (1, 5): False,
+                 (2, 0): False, (2, 1): True, (2, 2): 'Store', (2, 3): True, (2, 4): True, (2, 5): False,
+                 (3, 0): False, (3, 1): True, (3, 2): True, (3, 3): True, (3, 4): True, (3, 5): False,
+                 (4, 0): False, (4, 1): True, (4, 2): True, (4, 3): True, (4, 4): True, (4, 5): False,
+                 (5, 0): False, (5, 1): True, (5, 2): True, (5, 3): True, (5, 4): True, (5, 5): 'Gym'}
+        character = {'Character Name': 'user1', 'Money': 30, 'Current Level': 1, 'Potion': 0,
+                     'Current Location': (0, 0),
+                     'Starting Pokemon': 'Squirtle',
+                     'Poke Ball': {
+                         'Squirtle': {'type': 'water', 'currentHP': 40},
+                         'Pichu': {'type': 'electric', 'currentHP': 30},
+                         'Shinx': {'type': 'electric', 'currentHP': 30},
+                         'Mareep': {'type': 'electric', 'currentHP': 30},
+                         'Caterpie': {'type': 'grass', 'currentHP': 30},
+                         'Weedle': {'type': 'grass', 'currentHP': 30},
+                     }}
+        direction = 4
+        expected = (True, (0, 1))
+        actual = validate_move(board, character, direction)
+        self.assertEqual(expected, actual)
